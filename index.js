ab115c31e5de8db3272fd6115dd58350bc96d5fa
Jyotiranjan Patra <112277029+Jrpatra706@users.noreply.github.com>
2023-11-12 19:22:13 +0530

    A todo List
    todo list created using html, css , javascript

---
+++ index.js
@@ -0,0 +1,33 @@
+const arr=[];
+
+function showListOnHtml(){
+    let todoListHTML='';
+    for(let i=0;i<arr.length;i++){
+        const todo = arr[i];
+        const name = todo.name;
+        const dueDate =todo.dueDate;
+        const html = `
+            <p class="todo-txt">
+                ${name}
+            </p>
+            <p class="todo-txt" >
+                ${dueDate}
+            </p> 
+            <button onclick="
+                arr.splice(${i}, 1);
+                showListOnHtml();" class="delete-btn btn-delete">
+                Delete
+            </button>`;
+        todoListHTML += html;
+    }
+    document.querySelector('.todo').innerHTML = todoListHTML;
+}
+function addToList(){
+    const val = document.querySelector('.todo-name');
+    const dueDate = document.querySelector('.todo-date');
+    arr.push({
+        name :val.value,
+        dueDate:dueDate.value});
+    val.value='';
+    showListOnHtml();
+}
